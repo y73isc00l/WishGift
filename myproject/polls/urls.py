c4798a90e0195c09c55535e15b671a36d26f0b97
root <root@ubuntu-512mb-blr1-01.localdomain>
2017-01-14 09:20:16 +0000

    html site with image

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -1,6 +1,27 @@
-from django.conf.urls import url
-from . import views
+"""myproject URL Configuration
 
-urlpatterns=[
-	url(r'^$',views.index,name='index'),
-	]
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include,url
+from django.contrib import admin
+import views
+urlpatterns =[
+##      url(r'^home/$',home),
+        url(r'^$',views.home),
+        url(r'^index/$',views.index),
+        url(r'^time/plus/(\d{1,2})/$',views.timeafter),
+	url(r'^welcome/user/(\w+)/$',views.welcome),
+##      url(r'^polls/',include('polls.url')),##not working 
+    url(r'^admin/', admin.site.urls),
+]
